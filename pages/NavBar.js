c2ec0a7c2018276664a43a88d56900fad4fef63c
Harry Peach <harrypeach1999@gmail.com>
2020-03-22 12:48:46 +0000

    Started work on nav drawer and added scss

--- pages/NavBar.js
+++ pages/NavBar.js
@@ -3,15 +3,29 @@ import {
 	AppBar,
 	Toolbar,
 	Typography,
-	Button
+	Button,
+	IconButton
 } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 class NavBar extends React.Component {
 	render() {
 		return (
 			<AppBar position="static">
 				<Toolbar>
-					<Typography variant="h6">
+
+					{/* Only render the icon if it has been given a click handler */}
+					{
+						this.props.onIconClick !== undefined &&
+						<IconButton
+							color="inherit"
+							edge="start"
+							onClick={this.props.onIconClick}>
+							<MenuIcon />
+						</IconButton>
+					}
+
+					<Typography variant="h6" noWrap>
 						{this.props.title}
 					</Typography>
 				</Toolbar>
--- pages/feed.js
+++ pages/feed.js
@@ -2,12 +2,37 @@ import React from "react";
 import withAuth, { AuthContext } from "./authenticatedPage";
 import firebase from "../lib/firebase";
 import NavBar from "./NavBar";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import {
 	Container,
-	Box
+	Box,
+	Drawer,
+	List,
+	ListItem,
+	ListItemIcon,
+	ListItemText
 } from "@material-ui/core";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+
+const drawerWidth = 240;
 
 class Feed extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			leftDrawerOpen: false
+		};
+	}
+
+	handleLeftDrawerOpen = () => {
+		console.log("Opening Left Drawer");
+		this.setState({ open: true });
+	}
+
+	handleLeftDrawerClose = () => {
+		this.setState({ open: false });
+	}
+
 	signOut() {
 		console.log("Signing out!");
 		firebase.auth().signOut().then(function () {
@@ -17,11 +42,20 @@ class Feed extends React.Component {
 		});
 	}
 
+
 	static contextType = AuthContext;
 	render() {
 		return (
 			<React.Fragment>
-				<NavBar title="Social Network" />
+				<NavBar title="Social Network" onIconClick={this.handleLeftDrawerOpen} />
+				<Drawer variant="permanent">
+					<List>
+						<ListItem>
+							<ListItemIcon><InboxIcon /></ListItemIcon>
+							<ListItemText primary={"Profile"} />
+						</ListItem>
+					</List>
+				</Drawer>
 				<Container maxWidth="md">
 					<Box my={4}>
 						Welcome, {this.context}!
