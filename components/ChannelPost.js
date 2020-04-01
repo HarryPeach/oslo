3f72da12d3e38ea9dcb711995f295ad69d7e887b
Harry Peach <harrypeach1999@gmail.com>
2020-04-01 13:35:22 +0100

    Made channel cards clickable

--- components/ChannelPost.js
+++ components/ChannelPost.js
@@ -1,22 +1,26 @@
 import React, { useEffect } from "react";
-import Link from "next/link";
-import { Card, CardContent, Typography } from "@material-ui/core";
+import Router from "next/router";
+import { Card, CardContent, Typography, CardActionArea } from "@material-ui/core";
 import styles from "./ChannelPost.module.scss"
 
 export default function ChannelPost(props) {
+	const clickLink = () => {
+		Router.push("/channels/channel?c=" + props.name);
+	}
+
 	return (
 		<>
-			<Card className={styles.channelPost} elevation={3}>
-				<CardContent>
-					<Typography variant="h4">
-						<Link href={"channels/channel?c=" + props.name}>
+			<Card className={styles.channelPost} elevation={3} onClick={clickLink}>
+				<CardActionArea>
+					<CardContent>
+						<Typography variant="h4">
 							<a>#{props.name}</a>
-						</Link>
-					</Typography>
-					<Typography variant="subtitle1">
-						{props.description}
-					</Typography>
-				</CardContent>
+						</Typography>
+						<Typography variant="subtitle1">
+							{props.description}
+						</Typography>
+					</CardContent>
+				</CardActionArea>
 			</Card>
 		</>
 	);
