f9ea60f73a9f15bb22d6cd3a33c42b5d39d3fc3f
Harry Peach <harrypeach1999@gmail.com>
2020-04-01 17:07:39 +0100

    Added Link component backto ChannelPost

--- components/ChannelPost.js
+++ components/ChannelPost.js
@@ -1,27 +1,25 @@
 import React, { useEffect } from "react";
-import Router from "next/router";
+import Link from "next/link";
 import { Card, CardContent, Typography, CardActionArea } from "@material-ui/core";
 import styles from "./ChannelPost.module.scss"
 
 export default function ChannelPost(props) {
-	const clickLink = () => {
-		Router.push("/channels/channel?c=" + props.name);
-	}
-
 	return (
 		<>
-			<Card className={styles.channelPost} elevation={3} onClick={clickLink}>
-				<CardActionArea>
-					<CardContent>
-						<Typography variant="h4">
-							<a>#{props.name}</a>
-						</Typography>
-						<Typography variant="subtitle1">
-							{props.description}
-						</Typography>
-					</CardContent>
-				</CardActionArea>
-			</Card>
+			<Link href={`/channels/channel?c=${props.name}`}>
+				<Card className={styles.channelPost} elevation={3} >
+					<CardActionArea>
+						<CardContent>
+							<Typography variant="h4">
+								<a>#{props.name}</a>
+							</Typography>
+							<Typography variant="subtitle1">
+								{props.description}
+							</Typography>
+						</CardContent>
+					</CardActionArea>
+				</Card>
+			</Link>
 		</>
 	);
 }
